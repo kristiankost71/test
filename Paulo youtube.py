3de0a0141157cd1934c52f65c0c8734c85e691ab
Kristianko Kost <xkost@stuba.sk>
2023-08-08 16:14:13 +0200

    videjko 4, uz funkcne. Pozor medzera pri compile

--- Paulo youtube.py
+++ Paulo youtube.py
@@ -2,9 +2,10 @@ import py_dss_interface
 
 dss = py_dss_interface.DSSDLL(r"C:\Program Files\OpenDSS")
 
-dss_file = r"C:\Program Files\OpenDSS\IEEETestCases\13Bus\IEEE13Nodeckt.dss"
+dss_file = r"C:\Users\krist\Desktop\MODEE\OpenDSS\Hlavne-zadanie-Kost.txt"
 
-dss.text(f"compile[{dss_file}]")
+dss.text(f"compile [{dss_file}]")
 
-dss.text("Solve")
-dss.text("Show voltagesLN node")
+#dss.text("Solve")
+dss.solution_solve()
+dss.text("Show voltages LN node")
